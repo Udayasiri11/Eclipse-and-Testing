e928a8493e7371283e902d465c46df4cda372a37
Udayasiri11 <117638648+Udayasiri11@users.noreply.github.com>
2025-08-08 12:28:08 +0530

    Update FileReading.java

--- src/FileHandling/FileReading.java
+++ src/FileHandling/FileReading.java
@@ -1,10 +1,41 @@
 package FileHandling;
-
+import java.io.*;
 public class FileReading {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		{
+				BufferedReader reader=null;
+		 
+		try {
+			reader=new BufferedReader(new FileReader("text.pdf"));
+			String line;
+			while((line=reader.readLine()) !=null)
+			{
+				 System.out.println(line);
+			}
+		} 
+		catch (FileNotFoundException e) {
+			
+			System.out.println("File not Found :" + e.getMessage());
+		}
+		catch(IOException e)
+		{
+			System.out.println("An I/O error occured" + e.getMessage());
+		}
+		finally
+		{
+			try {
+				if(reader !=null)
+					reader.close();
+			}
+			catch(IOException e)
+			{
+				System.out.println("Error closing the reader :" + e.getMessage());
+			}
+		}
 
+			}
 	}
-
 }
+
